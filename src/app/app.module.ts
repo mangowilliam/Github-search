fef8c0637ef1e953fa3a7d09b3fcefb06e27b00c
williammango <williammango2015@gmail.com>
2019-06-07 11:29:11 +0300

    create github  component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GithubFormComponent } from './github-form/github-form.component';
+import { GithubComponent } from './github/github.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubFormComponent
+    GithubFormComponent,
+    GithubComponent
   ],
   imports: [
     BrowserModule
