d3ec13512c959863f55cecfebda0c5b78c190794
williammango <williammango2015@gmail.com>
2019-06-10 12:22:02 +0300

    update github component

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubserviceService } from "../githubservice.service";
 
 @Component({
   selector: 'app-github',
@@ -6,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
+  github: any[];
+  repos: any[];
+  gitname: string;
 
-  constructor() { }
-
+  constructor(private GithubserviceService: GithubserviceService) { }
+  getGit() {
+    this.GithubserviceService.updategitname(this.gitname);
+    this.GithubserviceService.getgitinfo().subscribe(Githubservice => {
+      console.log(Githubservice);
+      this.Githubservice = Githubservice;
+    })
+  }
   ngOnInit() {
   }
 
