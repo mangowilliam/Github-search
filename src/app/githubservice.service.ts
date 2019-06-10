2695502573ab7b9a3e6897307a56b4d7814b46b7
williammango <williammango2015@gmail.com>
2019-06-10 11:57:33 +0300

    solve service .map installations

--- src/app/githubservice.service.ts
+++ src/app/githubservice.service.ts
@@ -6,4 +6,23 @@ import "rxjs/add/operator/map";
   providedIn: 'root'
 })
 export class GithubserviceService {
+  private gitname: string;
+  private accesstoken = 'a1d58f9a608658bfff9369b3fe529d8bc5fa8986';
+
+
+  constructor(private http: Http) {
+    console.log("Service Ready")
+    this.gitname = "mangowilliam";
+  }
+  getgitinfo() {
+    return this.http.get("https://api.github.com/users/" + this.gitname + "?access_token=" + this.accesstoken)
+      .map(res => res.json)
+  }
+  getgitrepo() {
+    return this.http.get("https://api.github.com/users/" + this.gitname + "/repo?access_token=" + this.accesstoken)
+      .map(res => res.json());
+  }
+  updategitname(gitname: string) {
+  this.gitname = gitname;
+  }
 }
