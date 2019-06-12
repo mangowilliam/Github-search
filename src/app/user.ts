b36c9fd0d45e78ba35f753531a78d830858865c7
williammango <williammango2015@gmail.com>
2019-06-12 19:38:41 +0300

    Auto-generated commit

--- src/app/user.ts
+++
@@ -1,3 +0,0 @@
-export class Github {
-  constructor(public id: number, public login: string, public repos_url: string, public avatar_url: string, public url: string, public created_at: Date) { }
-}
